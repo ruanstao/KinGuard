8c64154873deda3a577fc03bf6b7ed44d59d2147
JK_iOS <Rainer@MacBook-Pro.local>
2016-05-04 22:52:59 +0800

    注册登录忘记密码
    注册登录忘记密码

---
+++ KinGuard/Module/Login/ViewController/ForgetViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgetViewController.h
+//  KinGuard
+//
+//  Created by Rainer on 16/5/4.
+//  Copyright © 2016年 RuanSTao. All rights reserved.
+//
+
+#import "KinViewController.h"
+
+@interface ForgetViewController : KinViewController
+
+@end
---
+++ KinGuard/Module/Login/ViewController/RegisterNickNameViewController.h
@@ -0,0 +1,13 @@
+//
+//  RegisterNickNameViewController.h
+//  KinGuard
+//
+//  Created by Rainer on 16/5/4.
+//  Copyright © 2016年 RuanSTao. All rights reserved.
+//
+
+#import "KinViewController.h"
+
+@interface RegisterNickNameViewController : KinViewController
+
+@end
