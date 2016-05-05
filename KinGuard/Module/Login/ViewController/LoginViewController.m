2981beb026103c1c7ef29b5bdb5c9bb9cac32dc0
JK_iOS <Rainer@MacBook-Pro.local>
2016-05-06 00:06:28 +0800

    登陆界面

--- KinGuard/Module/Login/ViewController/LoginViewController.m
+++ KinGuard/Module/Login/ViewController/LoginViewController.m
@@ -7,8 +7,12 @@
 //
 
 #import "LoginViewController.h"
+#import "ForgetViewController.h"
 
 @interface LoginViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *usernameField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordField;
 
 @end
 
@@ -17,6 +21,45 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.title = @"登录";
+    self.navigationItem.leftBarButtonItem = nil;
+}
+
+- (IBAction)login:(id)sender {
+    
+    NSString *phone = self.usernameField.text;
+    NSString *pwd = self.passwordField.text;
+    
+    if ([JJSUtil isBlankString:phone]) {
+        [JJSUtil showHUDWithMessage:@"请输入手机号码" autoHide:YES];
+        return;
+    }
+    if ([JJSUtil isBlankString:pwd]) {
+        [JJSUtil showHUDWithMessage:@"请输入密码" autoHide:YES];
+        return;
+    }
+    [[KinGuartApi sharedKinGuard] loginWithMobile:phone withPassword:pwd success:^(NSDictionary *data) {
+        [JJSUtil hideHUD];
+        [JJSUtil showHUDWithMessage:@"登陆成功" autoHide:YES];
+        
+        //跳转到主界面
+        
+    } fail:^(NSString *error) {
+        [JJSUtil hideHUD];
+        [JJSUtil showHUDWithMessage:error autoHide:YES];
+    }];
+}
+
+- (IBAction)forget:(id)sender {
+    NSString *phone = self.usernameField.text;
+    if (phone.length < 11 || [JJSUtil isBlankString:phone]) {
+        [JJSUtil showHUDWithMessage:@"请输入正确的手机号码" autoHide:YES];
+        return;
+    }
+    ForgetViewController *forgetController = [[UIStoryboard storyboardWithName:@"Main" bundle:nil] instantiateViewControllerWithIdentifier:@"ForgetVC"];
+    forgetController.phone = phone;
+    [self.navigationController pushViewController:forgetController animated:YES];
 }
 
 - (void)didReceiveMemoryWarning {
