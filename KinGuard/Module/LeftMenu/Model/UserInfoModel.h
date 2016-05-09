60c7c47fdec435e952ed84ed0de4736d26c95939
JK_iOS <Rainer@MacBook-Pro.local>
2016-05-10 00:35:05 +0800

    storyboard

---
+++ KinGuard/Module/LeftMenu/Model/UserInfoModel.h
@@ -0,0 +1,20 @@
+//
+//  UserInfoModel.h
+//  KinGuard
+//
+//  Created by RuanSTao on 16/5/8.
+//  Copyright © 2016年 RuanSTao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface UserInfoModel : NSObject
+@property (nonatomic,strong) NSString *acc;
+@property (nonatomic,strong) NSString *acc_name;
+@property (nonatomic,strong) NSString *addr;
+@property (nonatomic,strong) NSString *alias;
+@property (nonatomic,strong) NSString *birthdate;
+@property (nonatomic,strong) NSString *iddno;
+@property (nonatomic,strong) NSString *sex;
+
+@end
---
+++ KinGuard/Module/LeftMenu/View/LeftHeaderTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  LeftHeaderTableViewCell.m
+//  KinGuard
+//
+//  Created by RuanSTao on 16/5/6.
+//  Copyright © 2016年 RuanSTao. All rights reserved.
+//
+
+#import "LeftHeaderTableViewCell.h"
+
+@implementation LeftHeaderTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
