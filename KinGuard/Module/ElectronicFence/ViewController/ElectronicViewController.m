6b7be4d1fc1590fb9d5b1f434b846650955c5aea
JK_iOS <Rainer@MacBook-Pro.local>
2016-05-18 01:04:05 +0800

    电子围栏滑动删除 添加安全区域

--- KinGuard/Module/ElectronicFence/ViewController/ElectronicViewController.m
+++ KinGuard/Module/ElectronicFence/ViewController/ElectronicViewController.m
@@ -36,13 +36,21 @@
 
 - (void)initilizaBaseView
 {
-    self.safeTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, mScreenWidth, mScreenHeight - 64 - 49) style:UITableViewStylePlain];
+    self.safeTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, mScreenWidth, mScreenHeight - 49) style:UITableViewStylePlain];
     self.safeTableView.dataSource = self;
     self.safeTableView.delegate = self;
     self.safeTableView.separatorStyle = UITableViewCellSeparatorStyleNone;
     self.safeTableView.tableFooterView = [UIView new];
     self.safeTableView.backgroundColor = [UIColor colorWithRed:244/255.0 green:243/255.0 blue:239/255.0 alpha:1];
     [self.view addSubview:self.safeTableView];
+    
+    UIButton *addButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [addButton setFrame:CGRectMake(30, mScreenHeight - 44 - 20 - 49, mScreenWidth - 60, 44)];
+    [addButton setTitle:@"添加（最多五个）" forState:UIControlStateNormal];
+    [addButton setBackgroundColor:[UIColor colorWithRed:0 green:124/255.0 blue:195/255.0 alpha:1]];
+    [addButton.layer setCornerRadius:5];
+    [addButton addTarget:self action:@selector(addSafeZone:) forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:addButton];
 }
 
 - (void)requestData
@@ -111,6 +119,36 @@
     [cell.labTitle setText:safeModel.alias];
     [cell.labAddress setText:safeModel.addr];
     return cell;
+}
+
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+    return YES;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+//        [self.safeInfo removeObjectAtIndex:indexPath.row];
+        // Delete the row from the data source.
+//        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
+        
+    }
+}
+
+- (NSString *)tableView:(UITableView *)tableView titleForDeleteConfirmationButtonForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return @"删除";
+}
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:NO];
+}
+
+- (void)addSafeZone:(id)sender
+{
+    //跳转到添加安全区域界面
+    
 }
 
 - (void)didReceiveMemoryWarning {
