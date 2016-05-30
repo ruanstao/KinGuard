8f1a96ded2f8aebd5fd64706615bc7ea5b6c650c
JK_iOS <Rainer@MacBook-Pro.local>
2016-05-31 01:42:54 +0800

    设置界面生成二维码

---
+++ KinGuard/Module/Setting/ViewController/QRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QRCodeViewController.h
+//  KinGuard
+//
+//  Created by Rainer on 16/5/31.
+//  Copyright © 2016年 RuanSTao. All rights reserved.
+//
+
+#import "KinViewController.h"
+
+@interface QRCodeViewController : KinViewController
+
+@end
