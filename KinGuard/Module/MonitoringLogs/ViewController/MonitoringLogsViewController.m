023bc52356bb6b6550676038d466df0daef00734
JK_iOS <Rainer@MacBook-Pro.local>
2016-06-29 23:41:25 +0800

    监控日志

--- KinGuard/Module/MonitoringLogs/ViewController/MonitoringLogsViewController.m
+++ KinGuard/Module/MonitoringLogs/ViewController/MonitoringLogsViewController.m
@@ -7,11 +7,17 @@
 //
 
 #import "MonitoringLogsViewController.h"
+#import "LocationHistroyModel.h"
 
 @interface MonitoringLogsViewController ()
 @property (strong, nonatomic) IBOutlet UITableView *tableView;
 
 @property (nonatomic,strong) NSMutableArray *tableViewContent;
+
+@property (nonatomic, strong) NSMutableArray *systemContent;
+
+@property (nonatomic, assign) NSInteger selectIndex;
+
 @end
 
 @implementation MonitoringLogsViewController
@@ -20,6 +26,8 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.backBtn addTarget:self action:@selector(back) forControlEvents:UIControlEventTouchUpInside];
+    self.tableViewContent = [NSMutableArray array];
+    [self getdata];
 }
 
 - (void)back
@@ -33,7 +41,54 @@
 }
 
 - (IBAction)SegChange:(UISegmentedControl *)sender {
-    sender.selectedSegmentIndex = YES;
+    
+    self.selectIndex = sender.selectedSegmentIndex;
+    [self.tableView reloadData];
+}
+
+- (void)getdata
+{
+    NSDate *senddate = [NSDate date];
+    
+    NSDateFormatter *dateformatter=[[NSDateFormatter alloc] init];
+    
+    [dateformatter setDateFormat:@"YYYY-MM-dd"];
+    
+    //结束时间
+    NSString *endDate = [[dateformatter stringFromDate:senddate] stringByAppendingString:@"T24:00:00"];
+    
+    //开始时间
+    NSTimeInterval time = 30 * 24 * 60 * 60;//一年的秒数
+    NSDate * lastMonth = [senddate dateByAddingTimeInterval:-time];
+    //转化为字符串
+    NSString * startDate = [[dateformatter stringFromDate:lastMonth] stringByAppendingString:@"T00:00:00"];
+    
+    NSString *pid = [[NSUserDefaults standardUserDefaults] objectForKey:KinGuard_Device];
+    
+//    [[KinLocationApi sharedKinLocation] readLocationInfo:pid success:^(NSDictionary *data) {
+//        NSLog(@"%@",data);
+//        [[KinLocationApi sharedKinLocation] getLocationByPid:pid withLocationToken:[data objectForKey:@"token"] success:^(NSDictionary *data) {
+//            NSLog(@"%@",data);
+//        } fail:^(NSString *error) {
+//            
+//        }];
+//    } fail:^(NSString *error) {
+//        
+//    }];
+    
+    [[KinLocationApi sharedKinLocation] readPosHisInfo:pid withBegdt:startDate withEnddt:endDate success:^(NSDictionary *data) {
+        NSLog(@"device data:%@",data);
+        NSArray *array = (NSArray *)data;
+        self.systemContent = [array mutableCopy];
+        [array enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+            LocationHistroyModel *model = [LocationHistroyModel mj_objectWithKeyValues:obj];
+            [self.tableViewContent addObject:model];
+        }];
+        [self.tableView reloadData];
+    } fail:^(NSString *error) {
+        NSLog(@"device data:%@",error);
+        [JJSUtil showHUDWithMessage:error autoHide:YES];
+    }];
 }
 
 /*
@@ -67,25 +122,45 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
     [JJSUtil setExtraCellLineHidden:tableView];
-    return self.tableViewContent.count;
+    if (self.selectIndex == 0) {
+        return self.tableViewContent.count;
+    }
+    return self.systemContent.count;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [[self.tableViewContent objectAtIndex:section] count];
+    return 1;//[[self.tableViewContent objectAtIndex:section] count];
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath
 {
 
-    return 44;
+    if (self.selectIndex == 0) {
+        return 44;
+    }
+    return 144;
 
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
 //    LeftType type = [[[self.tableViewContent objectAtIndex:indexPath.section] objectAtIndex:indexPath.row] integerValue];
 
-        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MonitoringCell"];
-        return cell;
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"MonitoringCell"];
+    if (self.selectIndex == 0) {
+        LocationHistroyModel *model = [self.tableViewContent objectAtIndex:indexPath.section];
+        [cell.textLabel setText:model.location_m];
+        [cell.detailTextLabel setText:model.addr];
+    }else{
+        NSDictionary *body = [self.systemContent objectAtIndex:indexPath.section];
+        NSError *error = nil;
+        NSData *jsonData = [NSJSONSerialization dataWithJSONObject:body
+                                                           options:NSJSONWritingPrettyPrinted
+                                                             error:&error];
+        NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding:NSUTF8StringEncoding];
+        [cell setText:jsonString];
+    }
+    
+    return cell;
 
 }
 
